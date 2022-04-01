ae8941188e9493095efc4e15b918b38f8414e429
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-04-01 15:53:58 +0700

    learn methods

--- data/Person.php
+++ data/Person.php
@@ -5,4 +5,13 @@ class Person
     var string $name;
     var ?string $address = null; // nullable
     var string $country = "Indonesia"; // default value
+
+    function sayHello(?string $name)
+    {
+        if (is_null($name)) {
+            echo "Hi, my name is $this->name" . PHP_EOL;
+        } else {
+            echo "Hi $name, my name is $this->name" . PHP_EOL;
+        }
+    }
 }
---
+++ methods.php
@@ -0,0 +1,8 @@
+<?php
+require_once "data/Person.php";
+
+$pauzi = new Person();
+$pauzi->name = "Pauzi";
+
+$pauzi->sayHello("Pauzi");
+$pauzi->sayHello(null);
