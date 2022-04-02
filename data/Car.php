f5dcfbb954f7092965ce5a8b521d9ef3dd039e74
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-04-02 13:31:28 +0700

    learn interface inheritance

--- data/Car.php
+++ data/Car.php
@@ -1,18 +1,35 @@
 <?php 
 namespace Data;
 
-interface Car {
+interface HasBrand {
+    function getBrand():string;
+}
+
+interface IsMaintenance {
+    function isMaintenance():bool;
+}
+
+// interface Car extends HasBrand, isMaintenance {}
+interface Car extends HasBrand {
     function drive() : void;
     function getTire() : int;
 }
 
 // class BMW extends Manager implements Car, X, Y {}
-class BMW implements Car {
+class BMW implements Car, IsMaintenance {
     public function drive():void{
         echo "Drive BMW" . PHP_EOL;
     }
 
     public function getTire():int{
         return 4;
+    }
+
+    public function getBrand():string{
+        return "BWM blabla.."
+    }
+
+    public function isMaintenance():bool{
+        return false;
     }
 }
