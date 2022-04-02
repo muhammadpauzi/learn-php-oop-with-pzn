82c1a830e528105ac98f4a292518dae44d2b1715
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-04-02 18:28:22 +0700

    learn final method

--- data/SocialMedia.php
+++ data/SocialMedia.php
@@ -5,11 +5,29 @@ class SocialMedia
 }
 
 // cannot being parent of child classes anymore
-final class Facebook extends SocialMedia
+// final class Facebook extends SocialMedia
+// {
+// }
+
+class Facebook extends SocialMedia
 {
+    // cannot override by child classes
+    final public function login(string $username, $password): bool
+    {
+        return true;
+    }
 }
 
-// should be error
+// should be error (final class)
+// class FakeFacebook extends Facebook
+// {
+// }
+
 class FakeFacebook extends Facebook
 {
+    // should be error (final method)
+    // public function login(string $username, $password): bool
+    // {
+    //     return true;
+    // }
 }
