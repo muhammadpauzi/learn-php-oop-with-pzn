56af8e26b1ad6799c87f5d61942df327002ddce4
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-04-03 11:26:48 +0700

    learn annonymous class

---
+++ AnonymousClass.php
@@ -0,0 +1,15 @@
+<?php
+interface HelloWorld
+{
+    function sayHello(): void;
+}
+
+$helloWorld = new class implements HelloWorld
+{
+    public function sayHello(): void
+    {
+        echo "Hello World" . PHP_EOL;
+    }
+};
+
+$helloWorld->sayHello();
