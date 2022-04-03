aeabc3b678d091e91c1fd07f7ba68afabfcf2cec
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-04-03 12:03:29 +0700

    learn iterator

--- objectIteration.php
+++ objectIteration.php
@@ -1,11 +1,21 @@
 <?php
 
-class Data
+class Data implements IteratorAggregate
 {
     var string $first = "first";
     public string $second = "second";
     private string $third = 'third';
     protected string $fourth = "fourth";
+
+    public function getIterator(): Traversable
+    {
+        return new ArrayIterator([
+            "first" => $this->first,
+            "second" => $this->second,
+            "third" => $this->third,
+            "fourth" => $this->fourth,
+        ]);
+    }
 }
 
 $data = new Data();
